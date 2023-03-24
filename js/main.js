397db953fb2648f811c5adfd1f748505a365b1eb
Velasc94 <gvelasc94@gmail.com>
2023-03-23 22:27:22 -0300

    bordes en nav, colores modal y footer

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ class ControladorProductos {
                             <h5 class="card-title">${producto.nombre} ${producto.sabor} ${producto.presentacion}</h5>
                             <p class="card-text">${producto.descripcion}</p>
                             <p class = text-center><strong>$${producto.precio}</strong></p>
-                            <a href="#" class="btn btn-warning d-flex justify-content-center" id= "producto${producto.id}">Agregar al carrito</a>
+                            <a href="#" class="btn btn-warning d-flex justify-content-center botonAgregarCarrito" id= "producto${producto.id}">Agregar al carrito</a>
                         </div>
                     </div>
             `
@@ -63,10 +63,10 @@ class ControladorCarrito {
         this.listaCarrito.forEach(producto => {
             contenedor_carrito.innerHTML += `
             <section class="h-100 fondoCarrito">
-                            <div class="container h-100 py-2">
-                                <div class="card rounded-3 mb-1">
+                            <div class="container h-100 py-2 ">
+                                <div class="card rounded-3 mb-1" >
                                     <div class="card-body p-3">
-                                        <div class="row d-flex justify-content-between align-items-center">
+                                        <div class="row d-flex justify-content-between align-items-center" >
                                             <div class="col-md-2 col-lg-2 col-xl-2">
                                                 <img src=${producto.img} class="img-fluid rounded-start" alt=${producto.alt}>
                                             </div>
